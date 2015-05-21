c684b98c5eb6aa77698d8b74b39cc00fa4155b9d
Toru Kobayashi <koba0004@gmail.com>
2015-05-21 16:13:35 +0900

    for using unreleased build

---
+++ out/test/src/UnitTest/ESDocCLITest.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _assert = require('assert');
+
+var _assert2 = _interopRequireDefault(_assert);
+
+var _fsExtra = require('fs-extra');
+
+var _fsExtra2 = _interopRequireDefault(_fsExtra);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _srcESDocCLIJs = require('../../../src/ESDocCLI.js');
+
+var _srcESDocCLIJs2 = _interopRequireDefault(_srcESDocCLIJs);
+
+/** @test {ESDocCLI} */
+describe('ESDocCLI:', function () {
+
+  /**
+   * @test {ESDocCLI#exec}
+   * @test {ESDocCLI#_createConfigFromJSONFile}
+   */
+  it('can execute with config file.', function () {
+    var cliPath = _path2['default'].resolve('./src/cli.js');
+    var configPath = _path2['default'].resolve('./test/fixture/esdoc.json');
+    var argv = ['node', cliPath, '-c', configPath];
+    var cli = new _srcESDocCLIJs2['default'](argv);
+    cli.exec();
+    (0, _assert2['default'])(true);
+  });
+
+  /**
+   * @test {ESDocCLI#exec}
+   * @test {ESDocCLI#_createConfigFromPath}
+   */
+  it('can execute with directory path', function () {
+    var cliPath = _path2['default'].resolve('./src/cli.js');
+    var dirPath = _path2['default'].resolve('./test/fixture/src');
+    var argv = ['node', cliPath, dirPath];
+    var cli = new _srcESDocCLIJs2['default'](argv);
+    cli.exec();
+    _fsExtra2['default'].removeSync('./_esdoc_');
+    (0, _assert2['default'])(true);
+  });
+});
